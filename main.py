d9655fd4e60baa727df8838615664efd27ec9d0b
Ansidrake <siddhant10205@gmail.com>
2024-05-21 13:01:38 +0530

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import angel
+import creds
+
+angelone = angel(local_ip = creds.local_ip,public_ip = creds.public_ip,mac_address = creds.mac_address,api_key = creds.api_key)
+
+def login(angelone = angelone,creds = creds):
+    angelone.login(client_code = creds.client_code,client_pin = creds.client_pin,totp = creds.totp)
+
+# you can add any function now 
